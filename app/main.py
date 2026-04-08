2c9fb517889594b2d55aca4759778917154552c4
Zoe Nickson <mnickson@sidingsmedia.com>
2026-04-08 21:22:08 +0100

    Fixed linting issues
    Signed-off-by: Zoe Nickson <mnickson@sidingsmedia.com>

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 # SPDX-FileCopyrightText: 2026 Zoe Nickson <zoe.nickson@sidingsmedia.com>
 # SPDX-License-Identifier: MIT
 
+"""
+Main server entry point
+"""
+
 from fastapi import FastAPI
 
 
@@ -9,4 +13,11 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    """
+    Placeholder for the root API
+
+    Returns:
+        Placeholder Message
+    """
+
     return {"message": "Hello world"}
